65f0f8434bbd639a17b417961fadc989ec3d7d02
Arun Mopare <arunmopare546@gmail.com>
2021-09-17 14:32:36 +0530

    added socket in both frontend and backend

--- Frontend/chat-app/src/App.js
+++ Frontend/chat-app/src/App.js
@@ -1,8 +1,52 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { nanoid } from "nanoid";
+
+// no Dotenv
+const socket = io.connect("http://localhost:5000");
+const userName = nanoid(4);
 
 function App() {
-  return <div className="App"></div>;
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { message, userName });
+    setMessage("");
+  };
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        {chat.map((payload, index) => {
+          return (
+            <p key={index}>
+              <span style={{ color: "red" }}>{payload.userName + ":"}</span>{" "}
+              {payload.message},
+            </p>
+          );
+        })}
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="send Text"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
